22d3a60d1452224fe5a4cb6cbb9222dc6be73233
daengsik <daengsik@naver.com>
2024-02-08 10:13:40 +0900

    update code

--- ping_script.sh
+++ ping_script.sh
@@ -1,29 +1,37 @@
-#!/bin/sh
-ping_node=(
-192.168.0.61
-192.168.0.62
-192.168.0.63
-192.168.0.64
-)
-
+#!/bin/bash
+# CSV 파일 지정
 csv_file="testing_csv.csv"
+
+# CSV 파일 유무 확인
 if [ ! -f "$csv_file" ]; then
-	echo "CSV file dosen't exist $csv_file"
-	exit 1
+  echo "CSV file doesn't exist: $csv_file"
+  exit 1
 fi
 
-IFS=","
-while read -r field1 field2 field3; do
-	echo "field 1: $field1, field 2: $field2, field 3: $field3"
-done < "$csv_file"
+# 임시 파일 생성
+temp_file=$(mktemp)
 
-for list in ${ping_node[@]}
-do
-	ping -c 1 -W 1 "$list" > /dev/null
-	if [ $? -eq 0 ]; then
-	echo "node $list is up"
-	sleep 0.5
-	else
-	echo "node $list is down"
-	fi
+# IFS(,) 지정하여 필드 분리
+IFS=","
+
+# 파일을 읽어서 처리
+tail -n +2 "$csv_file" | while IFS=, read -r index ip_addr desc result; do
+  # ping 테스트 수행
+  ping -c 1 -W 1 "$ip_addr" > /dev/null
+
+  # ping 결과에 따라 결과 열 업데이트
+  if [ $? -eq 0 ]; then
+    result="OK"
+  else
+    result="FAIL"
+  fi
+
+  # 결과를 업데이트한 행을 임시 파일에 추가
+  echo "$index,$ip_addr,$desc,$result" >> "$temp_file"
+
+  # 결과 출력
+  printf "INDEX: %-5s, IPADDR: %-15s, DESC: %-20s, RESULT: %s\n" "$index" "$ip_addr" "$desc" "$result"
 done
+
+# 임시 파일을 원본 파일로 이동
+mv "$temp_file" "$csv_file"
